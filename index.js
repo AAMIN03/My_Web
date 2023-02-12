7b7edd502b5e33d3199024c51f3398a53e0ff72a
Aamin Chaudhari <aaminchaudhari@gmail.com>
2023-02-13 01:27:41 +0530

    HTTP-post()

--- index.js
+++ index.js
@@ -4,10 +4,23 @@ const port = 3000;
 
 app.use(express.json());
 
-app.post('/api/users', (req, res) => {
-  const user = req.body;
+// app.get('/',(req,res)=>{
+//   res.send("Hii there I'm Aamin");
+// })
 
-  // logic to store the user in a database
+app.post('/', (req, res) => {
+  const user = req.body;
+  console.log(user);
+  var ans=0;
+
+  if(user.operation=="sum"){
+    for(var i=0;i<user.values.length;i++)ans+=user.values[i];
+  }else if(user.operation=="multiply"){
+    ans=1;
+    for(var i=0;i<user.values.length;i++)ans*=user.values[i];
+  }
+
+  console.log(ans);
 
   res.status(201).send({ message: 'User created successfully' });
 });
