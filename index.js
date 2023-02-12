e8fea773bab666b01e905a794a6decce12de07ad
Aamin Chaudhari <aaminchaudhari@gmail.com>
2023-02-12 14:15:15 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+const port = 3000;
 
-app.get('/',(req,res)=>{
-  res.send("hii there !");
+app.get('/', function(req, res){
+  res.render('form');
+});
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+// for parsing application/json
+app.use(bodyParser.json()); 
+
+// for parsing application/xwww-
+app.use(bodyParser.urlencoded({ extended: true })); 
+//form-urlencoded
+
+// for parsing multipart/form-data
+app.use(upload.array()); 
+app.use(express.static('public'));
+
+app.post('/', function(req, res){
+  console.log(req.body);
+  res.send("recieved your request!");
 });
 
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
